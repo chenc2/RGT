92a4c23f0a79f2a67bf074231e69054d4186d5bc
chenc2 <chen.a.chen@intel.com>
2017-10-30 09:37:23 +0800

    Change PDO logic.
    Do not use create a new PDO file according to the old one.
    New the behavior modify the PDO file directly.

--- PDOLib.py
+++ PDOLib.py
@@ -1,56 +1,26 @@
 # -*- coding: UTF-8 -*-
-from ConfigLib import ConfigInfo
-import platform
-import os
-import shutil
 import CommonLib
 
 #
-#   Construct a new RepositoryInventory.pdo file by an exists PDO file.
+#   Modify the value by key in the PDO file.
 #
-def CreateNewRepositoryPDO(TheOldFile):
-    Lines = CommonLib.ReadFileList(TheOldFile)
-    
-    String = ''
-    for index in range(0,len(Lines)):
-        if Lines[index] == '[Globals]':
-            String = String + '[Globals]' + '\n'
-            String = String + Lines[index+1] + '\n'
-            String = String + Lines[index+2] + '\n'
-
-    assert(len(String) != 0)
-
-    String = String + '[Project_2]\n' + 'Path = ' + os.path.join(os.getcwd(),ConfigInfo.ProjectName)
-    CommonLib.WriteFile(os.path.join(ConfigInfo.PDOFileDir, 'PlatformProjectInventory.pdo'), String)
-
-#
-#
-#
-def MovePDO(Src, Dst, DeleteSrc = False):
-    if not os.path.isdir(Src):
-        assert(False)
-
-    if os.path.isdir(Dst):
-        shutil.rmtree(Dst)
-    elif os.path.isfile(Dst):
-        assert(False)
-    
-    shutil.copytree(Src,Dst)
-
-    if DeleteSrc:
-        shutil.rmtree(Src)
+def ModifyPDOFile(FilePath, Key, Value):
+    ContentList = CommonLib.ReadFileList(FilePath)
+    StringToWrite = ""
+    for Line in ContentList:
+        Buffer = Line.split('=')
+        if len(Buffer) == 2 and Buffer[0][:-1] == Key:
+            StringToWrite = StringToWrite + Buffer[0] + '= ' + Value + '\n'
+        else:
+            StringToWrite = StringToWrite + Line + '\n'
+    CommonLib.WriteFile(FilePath, StringToWrite)
 
 #
 #   Get the value of PDO file by one key-name.
 #
-def GetValueFromPDO(FileName, KeyName):
-    List = CommonLib.ReadFileList(os.path.join(ConfigInfo.PDOFileDir, FileName))
-    slash = '\\'
-    if 'Linux' in platform.system():
-        slash = '/'
-
-    for line in List:
+def GetValueFromPDO(FilePath, KeyName):
+    for line in CommonLib.ReadFileList(FilePath):
         if line.find(KeyName) == 0:
-                return line.split('=')[1][1:].strip('\n') + slash
+                return line.split('=')[1][1:].strip('\n')
 
     assert(False)
