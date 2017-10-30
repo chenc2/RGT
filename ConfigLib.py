88f7bec92cf6403e1904e0138aeb21d9f2c1512e
chenc2 <chen.a.chen@intel.com>
2017-10-30 09:33:49 +0800

    Change the method to save information.
    Use dict() variable to save configuare information.

--- ConfigLib.py
+++ ConfigLib.py
@@ -1,64 +1,51 @@
 # -*- coding: UTF-8 -*-
 import xml.dom.minidom
-import platform
 import os
+import InstallerLib
 
-#
-#   读取Config.xml配置文件。
-#
-class Config:
-    def __init__(self):
-        root = xml.dom.minidom.parse('Config.xml').documentElement
-        self.ExpectedDirName = root.getElementsByTagName('ExpectedDirName')[0].firstChild.data
-        self.ExpectedLogName = root.getElementsByTagName('ExpectedLogName')[0].firstChild.data
+ConfigInfo = dict()
 
-        self.TestDirName    = root.getElementsByTagName('TestDirName')[0].firstChild.data
-        self.TestLogName    = root.getElementsByTagName('TestLogName')[0].firstChild.data
-    
-        self.CompDirName    = root.getElementsByTagName('CompDirName')[0].firstChild.data
-        self.CompLogName    = root.getElementsByTagName('CompLogName')[0].firstChild.data
-        self.CompCSVName    = root.getElementsByTagName('CompCSVName')[0].firstChild.data
+def InitializeConfigXml():
+    SectionNameList = [
+        'ExpectedDirName',
+        'ExpectedLogName',
 
-        self.CmdFileName    = root.getElementsByTagName('CmdFileName')[0].firstChild.data
+        'TestDirName',
+        'TestLogName',
 
-        self.ProjectName    = root.getElementsByTagName('ProjectName')[0].firstChild.data
-        self.CaseRootTxt    = root.getElementsByTagName('CaseRootTxt')[0].firstChild.data
+        'CompDirName',
+        'CompLogName',
+        'CompCSVName',
 
-        if (    self.ExpectedDirName == "" or \
-                self.ExpectedLogName == "" or \
-                self.TestDirName == "" or \
-                self.TestLogName == "" or \
-                self.CompDirName == "" or \
-                self.CompLogName == "" or \
-                self.CmdFileName == "" or \
-                self.ProjectName == "" or \
-                self.CaseRootTxt == ""
-           ):
-            print "Please check config.xml file, it has error configuration."
+        'CmdFileName',
+
+        'ProjectName',
+        'CaseRootTxt',
+
+        'ExceptionDir',
+        'RGTRepository',
+
+        'TestCaseDir',
+        'BimxFileDir',
+        'ResultDir',
+    ]
+
+    root = xml.dom.minidom.parse('Config.xml').documentElement
+    for Name in SectionNameList:
+        ConfigInfo[Name] = root.getElementsByTagName(Name)[0].firstChild.data
+
+    ConfigInfo['PDOFileDir'] = InstallerLib.InstallerConfigInfo['PDOFileDir']
+
+    CheckDir = [
+        'TestCaseDir',
+        'BimxFileDir',
+        'ResultDir',
+        'RGTRepository'
+    ]
+    for Dir in CheckDir:
+        if not os.path.isdir(ConfigInfo.get(Dir)):
+            print ConfigInfo.get(Dir),'is an invalid path, please check.(ConfigLib)'
             assert(False)
 
-        if 'Linux' in platform.system():
-            self.PDOFileDir  = root.getElementsByTagName('LinPDOFileDir')[0].firstChild.data
-            if not os.path.isdir(self.PDOFileDir):
-                print "Please check the path of PDO files."
-                assert(False)
-        else:
-            self.PDOFileDir  = root.getElementsByTagName('WinPDOFileDir')[0].firstChild.data
-            if not os.path.isdir(self.PDOFileDir):
-                print "Please check the path of PDO files."
-                assert(False)
-
-        self.TestCaseDir    = root.getElementsByTagName('TestCaseDir')[0].firstChild.data
-        self.BimxFileDir    = root.getElementsByTagName('BimxFileDir')[0].firstChild.data
-        self.ResultDir      = root.getElementsByTagName('ResultDir')[0].firstChild.data
-        self.ExceptionDir   = root.getElementsByTagName('ExceptionDir')[0].firstChild.data
-
-        if (    not os.path.isdir(self.TestCaseDir) or \
-                not os.path.isdir(self.BimxFileDir) or \
-                not os.path.isdir(self.ResultDir) or 
-                not os.path.isdir(self.ExceptionDir)
-           ):
-            print "Please check config.xml file, it has error configuration."
-            assert(False)
-
-ConfigInfo = Config()
+def GetConfigInfo():
+    return ConfigInfo
